6dcb3598c7a4caa94aabb927dfdf7e95c37302f3
Dmytro Shyian <dimash0370@gmail.com>
2023-05-18 11:33:31 +0300

    Footer improvements

--- script.js
+++ script.js
@@ -31,17 +31,17 @@ function showSliderValue() {
 function onEntry(entry) {
   entry.forEach(change => {
     if (change.isIntersecting) {
-      change.target.classList.add('element-show');
+      change.target.classList.add("element-show");
     }
   });
 }
 
 let options = {
-  threshold: [0.5] };
+  threshold: [0.5],
+};
 let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.element-animation');
+let elements = document.querySelectorAll(".element-animation");
 
 for (let elm of elements) {
   observer.observe(elm);
 }
-
