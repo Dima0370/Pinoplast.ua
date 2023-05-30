5b851b2b98c7b78287349108ecdd4fc5ff368761
Dmytro Shyian <dimash0370@gmail.com>
2023-05-30 19:56:58 +0300

    New carusel

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
 });
 /*Карусель*/
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   var slides = document.getElementsByClassName("slide");
   var currentSlide = 0;
   var prevButton = document.querySelector(".prev");
@@ -133,3 +133,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
   nextButton.addEventListener("click", nextSlide);
   prevButton.addEventListener("click", prevSlide);
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
